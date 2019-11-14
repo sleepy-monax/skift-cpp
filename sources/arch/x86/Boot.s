bea6c1de01e36d779a1427835439abc2a93525ca
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2019-11-14 08:24:45 +0100

    arch-x86: Added basic support for multiboot

--- sources/arch/x86/Boot.s
+++ sources/arch/x86/Boot.s
@@ -1,6 +1,7 @@
 MBALIGN  equ  1 << 0
 MEMINFO  equ  1 << 1
-FLAGS    equ  MBALIGN | MEMINFO
+MBVIDEO  equ  0x4
+FLAGS    equ  MBALIGN | MEMINFO | MBVIDEO
 MAGIC    equ  0x1BADB002
 CHECKSUM equ -(MAGIC + FLAGS)
 
@@ -9,6 +10,20 @@ align 4
     dd MAGIC
     dd FLAGS
     dd CHECKSUM
+
+    ; MULTIBOOT_AOUT_KLUDGE
+    dd 0
+    dd 0
+    dd 0
+    dd 0
+    dd 0
+
+    ; MULTIBOOT_VIDEO_MODE
+    dd 1
+    dd 1280
+    dd 1024
+    dd 32
+
 
 section .bss
 align 16
@@ -21,6 +36,10 @@ global _start:function (_start.end - _start)
 _start:
     mov esp, stack_top
     extern arch_main
+
+    push ebx
+    push eax
+
     call arch_main
 
     cli
--- sources/arch/x86/Main.cpp
+++ sources/arch/x86/Main.cpp
@@ -1,20 +1,39 @@
+#include <libruntime/Types.h>
 #include <libsystem/Format.h>
 
 #include <arch/x86/CGAScreen.h>
 #include <arch/x86/SerialStream.h>
 #include <arch/x86/TerminalStream.h>
+#include <arch/x86/Multiboot.h>
 
-extern "C" void arch_main(void)
+using namespace libsystem;
+
+extern "C" void arch_main(u32 mboot_magic, multiboot_info_t *mboot_info)
 {
+    switch (mboot_info->framebuffer_type)
+    {
+    case MULTIBOOT_FRAMEBUFFER_TYPE_EGA_TEXT:
+        break;
+
+    case MULTIBOOT_FRAMEBUFFER_TYPE_INDEXED:
+        break;
+
+    case MULTIBOOT_FRAMEBUFFER_TYPE_RGB:
+        break;
+
+    default:
+        break;
+    }
+
     auto serial = SerialStream(SerialPort::COM1);
     auto terminal = TerminalStream(CGAScreen((void *)0xB8000));
 
-    while (true)
-    {
-        libsystem::print(terminal, "Hello world!\n");
-        libsystem::print(serial, "Hello world!\n");
+    print(terminal, "hjert kernel\n");
+    print(terminal, "--------------------------------------------------------------------------------\n");
+    print(terminal, "\n");
+    print(terminal, "Hi! I'm the new skiftOS kernel :) \n");
+    print(terminal, reinterpret_cast<const char *>(mboot_info->boot_loader_name));
 
-        libsystem::print(terminal, "Hello % world!\n", "other");
-        libsystem::print(serial, "Hello % world!\n", "other");
-    }
+    print(serial, "Hello world!\n");
+    print(serial, "Hello % world!\n", "other");
 }
