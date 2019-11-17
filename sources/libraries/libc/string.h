0a2ada5c2a0a9f290b585aed33d152761fcb4c65
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2019-11-17 12:57:50 +0100

    libc: Added mem* family of function.

--- sources/libraries/libc/string.h
+++ sources/libraries/libc/string.h
@@ -7,4 +7,12 @@ LIBC_BEGIN_HEADER
 
 size_t strlen(const char *str);
 
+void *memset(char *dest, int ch, size_t size);
+
+void *memcpy(char *dest, const char *src, size_t size);
+
+void *memmove(char *dest, const char *src, size_t size);
+
+int memcmp(const void *ptr1, const void *ptr2, size_t n);
+
 LIBC_END_HEADER
