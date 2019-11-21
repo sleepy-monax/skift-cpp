28a7bdfd980bbee3d185fb43b01b20c0165ff03b
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2019-11-21 21:00:05 +0100

    cxx-abi: Added __cxa_atexit.

--- sources/libraries/libsystem/__cxx__.cpp
+++ sources/libraries/libsystem/__cxx__.cpp
@@ -2,6 +2,32 @@
 #include <libruntime/Macros.h>
 #include <libsystem/Assert.h>
 
+#define MAX_AT_EXIT_FUNCTION 128
+
+struct AtExitFunctionEntry
+{
+    void (*destructor_func)(void *);
+    void *obj_ptr;
+    void *dso_handle;
+};
+
+static size_t __atexit_function_count = 0;
+static AtExitFunctionEntry __atexit_function[MAX_AT_EXIT_FUNCTION];
+
+void *__dso_handle;
+
+extern "C" int __cxa_atexit(void (*f)(void *), void *objptr, void *dso)
+{
+    assert(__atexit_function_count < MAX_AT_EXIT_FUNCTION);
+
+    __atexit_function[__atexit_function_count].destructor_func = f;
+    __atexit_function[__atexit_function_count].obj_ptr = objptr;
+    __atexit_function[__atexit_function_count].dso_handle = dso;
+    __atexit_function_count++;
+
+    return 0;
+};
+
 void *operator new(size_t size)
 {
     __unused(size);
