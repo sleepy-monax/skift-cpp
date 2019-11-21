a04c8621eb24a7383f4978cfe238b1c30b89510f
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2019-11-21 21:11:16 +0100

    arch-x86: Added arch::stop().

--- sources/arch/Arch.h
+++ sources/arch/Arch.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <libruntime/Types.h>
+#include <libruntime/Macros.h>
 
 namespace arch
 {
+
+void stop() __noreturn;
 
 size_t get_page_size();
 
--- sources/arch/x86/arch.cpp
+++ sources/arch/x86/arch.cpp
@@ -1,5 +1,13 @@
 #include <arch/Arch.h>
 
+void arch::stop()
+{
+    do
+    {
+        asm volatile("hlt");
+    } while (1);
+}
+
 size_t arch::get_page_size()
 {
     return 4096;
