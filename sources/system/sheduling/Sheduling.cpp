7d9624cc67b386c980f271da401286174f0bea0b
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2019-12-17 21:19:47 +0100

    system: Sheduler use locks for thread list.

--- sources/system/sheduling/Sheduling.cpp
+++ sources/system/sheduling/Sheduling.cpp
@@ -1,5 +1,7 @@
 #include <libruntime/LinkedList.h>
 #include <libsystem/Logger.h>
+#include <libsystem/__plugs__.h>
+#include <libruntime/SpinLock.h>
 
 #include "arch/Arch.h"
 #include "system/sheduling/Sheduling.h"
@@ -12,6 +14,7 @@ namespace system::sheduling
 
 static RefPtr<Thread> _running_thread;
 static LinkedList<RefPtr<Thread>> *_threads;
+libruntime::SpinLock _threads_lock;
 
 void initialize()
 {
@@ -22,18 +25,26 @@ void initialize()
 
 void register_thread(RefPtr<Thread> thread)
 {
+    _threads_lock.acquire();
+
     _threads->push_back(thread);
 
     if (_running_thread.necked() == NULL)
     {
-        logger_info("Using {} has running thread", thread.necked());
+        logger_info("Using {} has running thread", thread);
         _running_thread = thread;
     }
+
+    _threads_lock.release();
 }
 
 void unregister_thread(RefPtr<Thread> thread)
 {
+    _threads_lock.acquire();
+
     _threads->remove_all(thread);
+
+    _threads_lock.release();
 }
 
 libruntime::RefPtr<system::tasking::Thread> running_thread()
@@ -50,6 +61,12 @@ libruntime::RefPtr<system::tasking::Process> running_process()
     return _running_thread->process();
 }
 
+bool can_shedule()
+{
+    return !__plugs__::memory_is_lock() &&
+           !_threads_lock.is_acquired();
+}
+
 uintptr_t shedule(uintptr_t stack_pointer)
 {
     if (_running_thread.necked() == NULL)
@@ -57,11 +74,18 @@ uintptr_t shedule(uintptr_t stack_pointer)
         return stack_pointer;
     }
 
-    _running_thread->stack().set_pointer(stack_pointer);
+    if (can_shedule())
+    {
+        _running_thread->stack().set_pointer(stack_pointer);
 
-    _running_thread = _threads->peek_and_pushback();
+        _running_thread = _threads->peek_and_pushback();
 
-    return _running_thread->stack().get_pointer();
+        return _running_thread->stack().get_pointer();
+    }
+    else
+    {
+        return stack_pointer;
+    }
 }
 
 } // namespace system::sheduling
