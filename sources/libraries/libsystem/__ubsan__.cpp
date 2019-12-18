95ca9fb2c892986a85ad66be3de7f8cef711644e
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2019-12-18 10:53:18 +0100

    libsystem: Improved __ubsan_handle_type_mismatch_v1 handler.

--- sources/libraries/libsystem/__ubsan__.cpp
+++ sources/libraries/libsystem/__ubsan__.cpp
@@ -5,6 +5,9 @@
 #include <libruntime/Types.h>
 #include <libsystem/Logger.h>
 
+#define UBSAN_LOG(__args...) \
+    libsystem::log(libsystem::LogLevel::ERROR, data->location.filename, data->location.line, __args);
+
 struct UbsanSourceLocation
 {
     char *filename;
@@ -31,7 +34,20 @@ extern "C" void __ubsan_handle_type_mismatch_v1(UbsanTypeMismatchData_v1 *data, 
 {
     (void)ptr;
 
-    libsystem::log(libsystem::LogLevel::ERROR, data->location.filename, data->location.line, "Type mismatch");
+    if (ptr == 0)
+    {
+        UBSAN_LOG("Null pointer of type {} at {#x}", data->type->type_name, ptr);
+    }
+    else if ((data->log_alignment - 1) & ptr)
+    {
+        UBSAN_LOG("Miss aligned address {#x} of type {} wich requireds {} byte alignment", ptr, reinterpret_cast<char *>(&data->type->type_name[0]), data->log_alignment);
+    }
+    else
+    {
+        UBSAN_LOG("Not enought space at {#x} for {}", ptr, reinterpret_cast<char *>(&data->type->type_name[0]));
+    }
+
+    assert_not_reached();
 }
 
 struct UbsanInvalidValueData
