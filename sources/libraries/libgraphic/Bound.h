d379387951f5a21bbb36ce4a6d3fba673491c53d
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2019-12-20 19:21:11 +0100

    libgraphic: Removed useless inline keyword.

--- sources/libraries/libgraphic/Bound.h
+++ sources/libraries/libgraphic/Bound.h
@@ -14,34 +14,41 @@ private:
     double _height;
 
 public:
-    inline double X() { return _x; }
-    inline double Y() { return _y; }
-    inline double width() { return _width; }
-    inline double height() { return _height; }
+    double X() { return _x; }
+    double Y() { return _y; }
+    double width() { return _width; }
+    double height() { return _height; }
 
-    inline Point position() { return Point(_x, _y); }
-    inline Point size() { return Point(_width, _height); }
+    Point position() { return Point(_x, _y); }
+    Point size() { return Point(_width, _height); }
 
-    inline Point topleft() { return Point(_x, _y); }
-    inline Point topright() { return Point(_x + _width, _y); }
-    inline Point bottomleft() { return Point(_x, _y + _height); }
-    inline Point bottomright() { return Point(_x + _width, _y + _height); }
+    Point topleft() { return Point(_x, _y); }
+    Point topright() { return Point(_x + _width, _y); }
+    Point bottomleft() { return Point(_x, _y + _height); }
+    Point bottomright() { return Point(_x + _width, _y + _height); }
 
-    inline Bound(double x, double y, double width, double height) : _x(x),
-                                                                    _y(y),
-                                                                    _width(width),
-                                                                    _height(height)
+    Bound() : _x(0),
+              _y(0),
+              _width(0),
+              _height(0)
     {
     }
 
-    inline Bound(Point position, Point size) : _x(position.X()),
-                                               _y(position.Y()),
-                                               _width(size.X()),
-                                               _height(size.Y)
+    Bound(double x, double y, double width, double height) : _x(x),
+                                                             _y(y),
+                                                             _width(width),
+                                                             _height(height)
     {
     }
 
-    inline ~Bound() {}
+    Bound(Point position, Point size) : _x(position.X()),
+                                        _y(position.Y()),
+                                        _width(size.X()),
+                                        _height(size.Y)
+    {
+    }
+
+    ~Bound() {}
 };
 
 } // namespace libgraphic
